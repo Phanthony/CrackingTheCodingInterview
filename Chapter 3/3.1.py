b23881e2bb2b30c8833592a79b618ab653f509e8
aphan <anthony-phan@uiowa.edu>
2020-07-17 19:47:44 -0500

    3.1 & 3.2

---
+++ Chapter 3/3.1.py
@@ -0,0 +1,9 @@
+"""
+Three in one:
+Describe how you could use a single array to implement three stacks
+"""
+
+
+"""
+Divide the array by 1/3 and 2/3 to get the starting position of each stack
+"""
---
+++ Chapter 3/3.2.py
@@ -0,0 +1,11 @@
+"""
+Stack Min:
+How would you design a stack which, 
+in addition to push and pop, 
+has a function min which returns the minimum element?
+"""
+
+"""
+Have a class that does everything a regular stack already does,
+but also keeps track of the min element when adding in new elements.
+"""
